a2e1ea747c6ef0d9bf0e639d2e7517847ba57036
altai861 <altaizandanbileg@gmail.com>
2023-09-09 11:57:23 +0800

    Added most of backend

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const authControllers = require("../controllers/authControllers")
+
+router.route('/login')
+    .post(authControllers.login)
+
+router.route('/register')
+    .post(authControllers.register)
+
+router.route('/refresh')
+    .get(authControllers.refresh)
+
+router.route('/logout')
+    .get(authControllers.logout)
+
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,41 @@
+require('dotenv').config()
+const express = require('express')
+const path = require('path');
+const cookieParser = require("cookie-parser");
+const { logger } = require("./middleware/logger");
+const connectDB = require('./config/dbConn');
+const { default: mongoose } = require('mongoose');
+const app = express();
+const port = 3500;
+
+connectDB();
+
+// middlewares
+app.use(logger);
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use("/", require('./routes/root'))
+
+app.use('/auth', require('./routes/authRoutes'))
+
+app.all("*", (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found' })
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+})
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(port, () => console.log(`Server running on port ${port}`))
+})
+
+mongoose.connection.on('error', err => {
+    console.log(err)
+})
